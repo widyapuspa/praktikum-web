02b500c307d5da9f00ca8302de6b006d97a3683e
Widya Puspa <diedyawdz13@gmail.com>
2013-10-30 16:55:15 +0700

    praktikum ke-5

---
+++ 110533430605/Praktikum Ke-5/tugas rumah/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>Home</title>
+		<style type="text/css">
+			body{
+			background-color:#00CC00;
+			}
+			.menu{
+				text-align:center;
+				font-size:24px;
+			}
+		</style>
+	</head>
+
+	<body>
+		<h1 style="text-align:center;">Selamat Datang Di Aplikasi Database</h1>
+		<div class="menu">
+			<a href="./seleksi">Seleksi Data disini</a>
+			<a href="./login">Login Admin Disini</a>
+		</div>
+	</body>
+</html>
---
+++ 110533430605/Praktikum Ke-5/tugas rumah/login/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>Halaman Administrator</title>
+		<style type="text/css">
+			body{
+				background-color:#00CC00;
+				color:#FFFFFF;
+			}
+			.inner{
+				margin: 200px auto;
+				padding: 20px;
+				width:240px;
+				border:1px solid #333
+			}
+		</style>
+	</head>
+
+	<body>
+		<?php
+			//include file eksternal
+			require_once('./proses.php');
+			
+			init_login();
+			validate();
+		?>
+		<script type="text/javascript">
+			document.location.href="../admin";
+		</script>
+	</body>
+</html>
